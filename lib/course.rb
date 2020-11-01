93633ea0ea21455e322f6c958e79bca1c795630b
nrymarz <nrymarz@gmail.com>
2020-11-01 16:00:48 -0600

    commit tree aa6f67772fc636d6d95409507e97a6396fe79a1d

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,16 @@
+class Course
+    @@all = []
+    def initialize
+        @@all << self
+    end
+    attr_accessor :title, :description, :schedule
+    def self.all
+        @@all
+    end
+    def self.reset_all
+        @@all.clear
+    end
+end
+
 
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'pry'
 require 'open-uri'
 
 require_relative './course.rb'
@@ -15,8 +16,23 @@ class Scraper
       end
     end
   end
-  
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+  def get_courses
+    self.get_page.css(".post")
+  end
+  def make_courses
+    self.get_courses.each do |course|
+      new_course = Course.new
+      new_course.title = course.css("h2").text
+      new_course.schedule = course.css(".date").text
+      new_course.description = course.css("p").text
+    end
+  end
 end
+
+
 
 
 
